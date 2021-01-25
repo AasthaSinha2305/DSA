1e94f6b2e64bb27bdba18d8c5b85b6e4f71ea6b0
AasthaSinha2305 <55781193+AasthaSinha2305@users.noreply.github.com>
2021-01-26 02:07:48 +0530

    new algo added

---
+++ Algos/gcd(lcm).cpp
@@ -0,0 +1,35 @@
+//* bitwise gcd*//
+int swap(int *a , int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int gcd(int a, int b) {
+    if (!a || !b)
+        return a | b;
+    unsigned shift = __builtin_ctz(a | b);
+    a >>= __builtin_ctz(a);
+    do {
+        b >>= __builtin_ctz(b);
+        if (a > b)
+            swap(&a, &b);
+        b -= a;
+    } while (b);
+    return a << shift;
+}
+
+//** Normal gcd **//
+
+int gcd (int a, int b) {
+    if (b == 0)
+        return a;
+    else
+        return gcd (b, a % b);
+
+}
+
+//** Lcm **//
+int lcm (int a, int b) {
+    return a / gcd(a, b) * b;
+}
